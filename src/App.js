0d6651bf334bccf42a7e61795bd0fcefee586fed
brenomorais-ti <brenowebdesigner@gmail.com>
2022-12-02 22:15:19 -0300

    add task

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css"
 import Navbar from "./components/Navbar/Navbar";
 import TaskList from "./components/TaskList/TaskList";
 
-function App() {  
+function App() {
+  let idAcc = 0;
+  const generateId = () => {
+    idAcc = idAcc + 1;
+    return idAcc;
+  }
+
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTask) => {
+      console.log("executando de setTaskList")
+      return [... existingTask, newTask];
+    });
+  };
+
   return (
     <div className="App">
       
       <Navbar />
 
       <div className="container">
-        <TaskList title="Pendente"/>
-        <TaskList title="Fazendo"/>
-        <TaskList title="Completa"/>
+        <TaskList title="Pendente" onAddTask={addTask} tasks={tasks}/>
       </div>      
     </div>
   );
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,11 +1,25 @@
 import React from "react";
 import './tasklist.css';
+import { useState } from "react";
 
-function TaskList( { title } ) {
+function TaskList( { title, onAddTask, tasks } ) {
+
+    const addTask = () => {
+        console.log("executando de addTask")
+        onAddTask('Nova Tarefa', 'Pendente');
+    }
+ 
     return(
         <div className="tasklist">
             <div className="title">{ title }</div>
-            <div className="content"></div>
+            <div className="content">
+                {tasks.map((task) => {
+                    return <div key={task.id}> {task.title} </div>
+                })}
+            </div>
+            <button onClick={addTask}>Incremento</button>
+            
+
         </div>
     );
 }
